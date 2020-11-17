b23345645c480351288b3223676ce7e6368f898f
taruvar-mittal <mittaltaruvar@gmail.com>
2020-11-17 20:03:03 +0530

    minor change

--- my-website/src/components/Navbar.js
+++ my-website/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
          <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                    TRVL {" "} <i className="fab fa-typo3"></i>
+                    TRVL {" "}<i className="fab fa-typo3"></i>
                 </Link>
             </div> 
          </nav>
