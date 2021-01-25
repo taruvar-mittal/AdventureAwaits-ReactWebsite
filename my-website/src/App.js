2a9d1b0e28be18c1a6022cdbdde0601638813891
taruvar-mittal <mittaltaruvar@gmail.com>
2021-01-25 20:05:30 +0530

    cards added

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import Navbar from './components/Navbar';
 import './App.css';
-
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" exact />
+        <Route path="/" exact component={Home}/>
       </Switch>
     </Router>
     </>
